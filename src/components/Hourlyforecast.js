76855f346da0e367e42679b992dd0d1cb85c4f9c
Vaibhav Kumar <vaibhav11kr@gmail.com>
2024-01-01 00:42:51 +0530

    last commit

--- src/components/Hourlyforecast.js
+++ src/components/Hourlyforecast.js
@@ -5,11 +5,11 @@ const Hourlyforecast = ({weather}) => {
   const containerClass = "bg-[#202B3B] rounded-lg mt-4 p-4";
   const headingClass = "text-xs text-[#9399A2] text-center";
   const innerContainerClass = "flex whitespace-nowrap overflow-x-auto mt-2";
-  const itemContainerClass = "flex flex-col flex-grow justify-center items-center pr-3 h-16 w-40 m-2 border-r-2 border-[#35445D]";
+  const itemContainerClass = "flex flex-col flex-grow justify-center items-center pr-3 h-25 w-40 m-2 border-r-2 border-[#35445D]";
   const timeClass = "text-[#9399A2]";
   const temperatureClass = "text-[#DEE0E4]";
   const imageClass = "h-8 w-8";
-  
+
   return (
     <div className={containerClass}>
       <h2 className={headingClass}>TODAY'S FORECAST</h2>
--- src/components/Weeklyforecast.js
+++ src/components/Weeklyforecast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import real_feel from '../assets/cloud.png';
+import { iconUrlFromCode } from "../services/weatherServices";
 
 const Weeklyforecast = ({ weather }) => {
   console.log(weather);
@@ -11,8 +11,8 @@ const Weeklyforecast = ({ weather }) => {
       {weather.map((day, index) => (
         <div key={index} className="flex flex-row items-center m-3 justify-between flex-grow pb-3 border-b-2 border-[#35445D]">
           <p className='text-[#9399A2]'>{day.title}</p>
-          <div className="flex items-center h-11 w-11 justify-center">
-            <img src={real_feel} className='mr-2' alt="" />
+          <div className="flex h-11 w-16 justify-between items-center">
+            <img src={iconUrlFromCode(day.icon)} className='mr-2' alt="" />
             <h3 className='text-[#C4CAD3]'>{day.main}</h3>
           </div>
           <p className='text-[#C4CAD3]'>{day.temp}° C</p>
